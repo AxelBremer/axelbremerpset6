8ca3454178926ffbdf83624e794c8cc7baa9b7c0
AxelBremer <axelbremer94@gmail.com>
2017-12-07 14:18:45 +0100

    Added login page design and firebase start

--- app/src/main/java/axelbremer/axelbremerpset6/FirstActivity.java
+++ app/src/main/java/axelbremer/axelbremerpset6/FirstActivity.java
@@ -1,13 +1,48 @@
 package axelbremer.axelbremerpset6;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FirstActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    String email;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void createUser() {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("create user", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("create user", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(FirstActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
